c9a517a8ea3961d492e247017ed9ec4e70422ecc
Faris Al-khatahtbeh <48871391+farisalkhat@users.noreply.github.com>
2022-12-14 00:17:48 -0500

    .

---
+++ scripts/PlayableCharacter.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+export(int) var hp
+export(int) var tp
+
+export(int) var atk
+export(int) var def
+export(int) var matk
+export(int) var mdef
+export(int) var spd
+export(int) var luck
+
+export var charactername="player"
+
+func _ready():
+	pass # Replace with function body.
+	
+	
+
---
+++ scripts/battle.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+
+var players = []
+var enemies = []
+
+var queue=[]
+var curr_round = 0
+
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
